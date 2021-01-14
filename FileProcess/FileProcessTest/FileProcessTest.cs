eac9482a6e9c515459b0b43dac5f3d8da05120c8
TulioliAles <desmecad@hotmail.com>
2021-01-14 15:57:38 -0300

    Iniciado os projetos Principal e de Tests

---
+++ FileProcess/FileProcessTest/FileProcessTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FileProcessTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExists()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExists()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
